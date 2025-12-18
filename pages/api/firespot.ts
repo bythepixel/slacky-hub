81ab092d6566ad38a0d4c6427d20e9e7b2c1732a
Turner Walters <turner@bythepixel.com>
2025-12-18 16:30:39 -0500

    update firespot endpoint for post

--- pages/api/firespot.ts
+++ pages/api/firespot.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { validateMethod } from '../../../lib/utils/methodValidator'
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (!validateMethod(req, res, ['POST'])) return
+
     return res.status(200).json({ status: 'OK' })
 }
 
